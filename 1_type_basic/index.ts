c386395d2fdaa8c90781324a45d1c0447a5062f9
baekCode <baekcode@gmail.com>
2020-12-01 00:02:32 +0900

    add : 함수타입, 파리미터와 리턴값 타입정의

--- 1_type_basic/index.ts
+++ 1_type_basic/index.ts
@@ -9,7 +9,7 @@ const array2Ts: Array<string> = ['aaa', 'bbb', 'ccc']
 const array3Ts: number[] = [1, 2, 3, 4]
 const array4Ts: string[] = ['a', 'b', 'c']
 
-// TS 튜플 : 배열 요소의 순서에 따른 타입 정의, 배열의 인덱스의 타입을 정의
+// TS 튜플 : 배열 요소의 순서에 따른 타입 정의, 배열의 인덱스의 타입을 정
 const twoArray: [string, number] = ['abc', 123]
 
 // TS 객체
@@ -25,3 +25,9 @@ const person2: { name: string, age: number } = {
 
 // TS 불리언
 let isSHow: boolean = true
+
+// TS 함수_파라미터 정의
+const sum = (a:number, b:number) => a + b;
+
+// TS 함수_리턴 정의
+const add = (a:number, b:number):number => a + b;
